7d5e7e5c340a1860e17a4cdca6bff28aaa80546b
Jeff Brown <jdotcafe@gmail.com>
2021-03-17 22:15:29 -0500

    deleted unnecessary files

--- wavy_bert/run.py
+++ wavy_bert/run.py
@@ -1,9 +1,32 @@
+
+from collections import OrderedDict
+from itertools import chain
+
+import h5py
+import math
+import json
+import torch
+import wandb
+import numpy as np
+import pytorch_lightning as pl
+from glob import glob
+import librosa
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch.nn.functional as F
+import random
+from sklearn.metrics import f1_score
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import WandbLogger
+
+
 from wavy_bert import *
 
 WANDB_NAME = 'jefe-jeff' 
 
 config = {
-    'datapath':"/Users/jbrown/Documents/CS224NS_Data",
+    'datapath':"/content/gdrive/MyDrive/CS224NS_Data/",
     'p' :0.15,
     'lr' : 4e-5, 
     'lr_decay' : 0.96,
@@ -11,10 +34,10 @@ config = {
     'lambda_AC': 1,
     'lambda_LM' : 0.2,
     'max_label_length' : 400,
-    'max_w2v_length' : 400,
+    'max_w2v_length' : 16000*15,
     'num_warmup_steps' : 8000,
     'num_train_steps' :42000,
-    'batch_size': 16
+    'batch_size': 1
 }
 
 run(system="LightningWavyBert", config=config, ckpt_dir='wavy-bert', epochs=20, 
